cecccd5fbf9fe380bf96c128f0268de299893ac2
Yu Yagihashi <yagihashoooooo@gmail.com>
2014-02-11 22:35:40 +0900

    タイマー完成

---
+++ timer/index.php
@@ -0,0 +1,38 @@
+<?php
+// for debugging
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', '1');
+
+$headers = array("Expires" => "-1", "Content-Type" => "text/html; charset=UTF-8", "Content-Security-Policy" => "default-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'", "X-Frame-Options" => "DENY", "X-Content-Type-Options" => "nosniff", "X-XSS-Protection" => "1; mode=block");
+foreach ($headers as $key => $value)
+  header("{$key}: {$value}");
+?>
+<!DOCTYPE html>
+
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <title>RG LT Timer</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/jquery-1.11.0.min.js"></script>
+    <script src="js/script.js"></script>
+  </head>
+
+  <body>
+    <div id="wrap">
+      <div class="time_rest">
+        <span id="presenter"></span>さんが話しています
+        <br />
+        <span class="time_rest">05'00"</span>
+      </div>
+      <div class="controll">
+        <form class="controll">
+          <label>ログイン名を入力して送信
+            <br />
+            <input type="text" id="login_name" placeholder="ログイン名" pattern="^[A-Za-z0-9]{1,10}$" maxlength="10" size="10" autofocus required />
+          </label>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ timer/js/script.js
@@ -0,0 +1,65 @@
+$(function() {
+  var loop;
+  $("form.controll").submit(function() {
+    if(typeof loop !== "undefined")
+      clearInterval(loop);
+    var loginName = $("input#login_name").val();
+    $("span#presenter").text(loginName);
+    loop = startTimer();
+    return false;
+  });
+
+  function prepareSound() {
+    // Thanks for くらげ工匠(http://www.kurage-kosho.info/)
+    var audio = new Audio("");
+    var canPlayWav = (audio.canPlayType("audio/wav") == "maybe");
+    if (canPlayWav) {
+      audio.src = "sounds/cym03.wav";
+    } else {
+      audio.src = "sounds/cym03.mp3";
+    }
+    return audio;
+  }
+
+  function startTimer(formatted_time) {
+    $("span.time_rest").css("color", "#000000");
+    var sound = prepareSound();
+    var sec = min2sec(formatted_time) || 300;
+    var min = sec2min(sec);
+    $("span.time_rest").text(min);
+    var loop = setInterval(function() {
+      $("span.time_rest").text(sec2min(--sec));
+      if (sec == 0) {
+        clearInterval(loop);
+        sound.play();
+      } else if(sec == 60) {
+        $("span.time_rest").css("color", "#DD0000");
+      }
+    }, 1000);
+    return loop;
+  }
+
+  // 秒数を渡す
+  // xx'xx"の形式で時間を返す"
+  function sec2min(sec) {
+    if (isNaN(sec))
+      return false;
+    var min = ("0" + (sec / 60 | 0)).slice(-2);
+    var sec = ("0" + (sec % 60 | 0)).slice(-2);
+    return min + "'" + sec + '"';
+  }
+
+  // xx'xx"の形式で時間を渡す
+  // 秒数を返す
+  function min2sec(formatted) {
+    var formatted = formatted || "";
+    if (!formatted.match(/^[0-9]{2}'[0-9]{2}"$/))
+      return false;
+    var min = parseInt(formatted.match(/^([0-9]{2})/)[1]);
+    var sec = parseInt(formatted.match(/([0-9]{2})"/)[1]);
+    if (sec > 59)
+      return false;
+    return min * 60 + sec;
+  }
+
+});
