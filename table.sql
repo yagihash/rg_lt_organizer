9297558a80adf779f21386b0d70dd3953421e5e7
root <root@dti-vps-srv728.(none)>
2014-03-08 20:58:29 +0900

    テーブルの検討開始

---
+++ table.sql
@@ -0,0 +1,41 @@
+DROP IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id`  BIGINT(10) NOT NULL AUTO_INCREMENT,
+  -- `ldapunique` BIGINT(10) NOT NULL,
+  `login_name`   VARCHAR(12) NOT NULL,
+  `screen_name`  VARCHAR(30) NOT NULL,
+  `kg_id`           INT(3) NOT NULL,
+  `biography`    TEXT, 
+  `timeadded` TIMESTAMP, -- 自動でタイムスタンプ追加
+  PRIMARY KEY (`id`)
+);
+
+DROP IF EXISTS `talks`;
+CREATE TABLE `talks` (
+  `id` BIGINT(10) NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT(10) NOT NULL,
+  FOREIGN KEY (`user_id`)
+  REFERENCES users(`id`),
+  `slides` VARCHAR(500), -- スライドのURL
+  `week_id` INT(3),
+  FOREIGN KEY (`week_id`)
+  REFERENCES lt_weeks(`id`),
+  `timeadded` TIMESTAMP, -- 自動でタイムスタンプ追加
+  PRIMARY KEY (`id`)
+);
+
+DROP IF EXISTS `lt_weeks`;
+CREATE TABLE `lt_weeks` ( -- LTを実施する週、日付を設定。管理メニューから編集。
+  `id` INT(3) NOT NULL AUTO_INCREMENT,
+  `week` INT(2), -- 何週目
+  `date` DATE, -- 日付
+);
+
+DROP IF EXISTS `kg`;
+CREATE TABLE `kg` (
+  `id` INT(3) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
