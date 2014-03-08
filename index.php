241069bd04e0a3d7d139aaa648e78aa38b1ad5f5
Yu Yagihashi <yagihashoooooo@gmail.com>
2014-03-08 20:24:28 +0900

    　

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+    <title>RG-LT</title>
+  </head>
+  
+  <body>
+    <?php
+      echo "number_format()は便利！\n";
+      echo number_format(1000000);
+    ?>
+  </body>
+</html>
