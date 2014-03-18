61cd6915763e7b0e6db98ea85bd13576641651f6
Yu Yagihashi <yagihashoooooo@gmail.com>
2014-03-18 12:06:26 +0900

    個人ページ関連のファイル追加

---
+++ edit.php
@@ -0,0 +1,23 @@
+<?php
+require_once (__DIR__ . "/common.php");
+?>
+<!DOCTYPE html>
+
+<html lang="ja">
+  <head>
+<?php
+require_once (__DIR__ . "/head.php");
+?>
+  </head>
+
+  <body>
+<?php
+require_once(__DIR__ . "/top_bar.php");
+?>
+    <div id="wrap">
+<?php
+require_once(__DIR__ . "/page_header.php");
+?>
+    </div>
+  </body>
+</html>
