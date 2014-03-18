edbaebfc2fd0c9ccf88919bcc880c8a8c25a21b4
Yu Yagihashi <yagihashoooooo@gmail.com>
2014-03-18 12:03:01 +0900

    ひろたん頑張れ

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function() {
-  $("nav#top_bar ul").css("margin-left", $("div#wrap").css("margin-left"));
+  $("nav#top_bar ul").css("margin-left", (window.innerWidth - 800) / 2 + "px")
   $(window).resize(function() {
-    $("nav#top_bar ul").css("margin-left", $("div#wrap").css("margin-left"));
+    $("nav#top_bar ul").css("margin-left", (window.innerWidth - 800) / 2 + "px")
   });
   $("nav#top_bar ul li a").hover(function() {
     $(this).stop().animate({
@@ -13,4 +13,18 @@ $(function() {
       backgroundColor : "#C4E9E0"
     }, 300);
   });
+  
+  /* * /
+  $("form#register input[type=button]").click(function() {
+    var paramCheck = true;
+    $("form#register select, form#register input[type=text], form#register input[type=file]").each(function() {
+      if (this.value === "")
+        paramCheck = false;
+    });
+    if (paramCheck && window.confirm("送信します。"))
+      $("form#register").submit();
+    else
+      alert("入力項目を確認してください。");
+  });
+  /* */
 });
--- register.php
+++ register.php
@@ -12,47 +12,58 @@ require_once (__DIR__ . "/head.php");
 
   <body>
 <?php
-require_once(__DIR__ . "/top_bar.php");
+require_once (__DIR__ . "/top_bar.php");
 ?>
     <div id="wrap">
 <?php
-require_once(__DIR__ . "/page_header.php");
+require_once (__DIR__ . "/page_header.php");
 ?>
       <div id="main" class="content">
 <?php
 if($isAuthed) {
 ?>
         <h2>Register</h2>
-        <form method="POST">
+        <form id="register" method="POST">
           <input type="hidden" name="token" value="<?php echo issueToken(); ?>" />
-          <label>Title: <input type="text" name="title" /></label>
-          <label>Slide: <input type="file" name="slide" accept="application/pdf" /></label>
-          <label>スライドをこのページで公開する<input type="checkbox" name="publish_slide" /></label>
-          <label>KG: <select name="kg">
-            <option>-----</option>
-          	<option value="isc">ISC</option>
-          	<option value="hoge">HOGE</option>
-          	<option value="fuga">FUGA</option>
-          	<option value="piyo">PIYO</option>
+          <label><span>Date:</span><select name="date" required>
+            <option value="1">4/10</option>
+            <option value="2">4/17</option>
+            <option value="3">4/24</option>
+            <option value="4">5/1</option>
+            <option value="5">5/8</option>
           </select></label>
-          <label>Year: <select name="year"><!-- TODO: 一度登録すると以降は勝手に学年選んでくれるように -->
-          	<option value="b1">B1</option>
-          	<option value="b2">B2</option>
-          	<option value="b3">B3</option>
-          	<option value="b4">B4</option>
-          	<option value="m1">M1</option>
-          	<option value="m2">M2</option>
-          	<option value="faculty">Faculty</option>
-          	<option value="Other">Other</option>
+          <label><span>Title:</span><input type="text" name="title" placeholder="Ex.) キャベツとレタス、どっちがセキュア？" maxlength="100" required /></label>
+          <label><span>Slides:</span><input type="file" name="slide" accept="application/pdf" required /></label>
+          <label class="check">スライドをこのページで公開する<input type="checkbox" name="publish_slide" checked /></label>
+          <label><span>KG:</span><select name="kg" required>
+            <option value="">-----</option>
+            <option value="isc">ISC</option>
+            <option value="hoge">HOGE</option>
+            <option value="fuga">FUGA</option>
+            <option value="piyo">PIYO</option>
           </select></label>
+          <label><span>Year:</span><select name="year" required><!-- TODO: 一度登録すると以降は勝手にKGと学年選んでくれるように -->
+            <option value="">-----</option>
+            <option value="b1">B1</option>
+            <option value="b2">B2</option>
+            <option value="b3">B3</option>
+            <option value="b4">B4</option>
+            <option value="m1">M1</option>
+            <option value="m2">M2</option>
+            <option value="faculty">Faculty</option>
+            <option value="other">Other</option>
+          </select></label>
+          <input type="submit" value="Submit" />
         </form>
 <?php
-} else {
+        } else {
 ?>
         <h2>Error</h2>
-        <p>Please login to register.</p>
+        <p>
+          Please login to register.
+        </p>
 <?php
-}
+        }
 ?>
       </div>
     </div>
