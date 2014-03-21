89687b4b36e1e68d3658d7fe21940e131bfc02b7
Yu Yagihashi <yagihashoooooo@gmail.com>
2014-03-21 22:49:26 +0900

    hogehoge

--- js/script.js
+++ js/script.js
@@ -5,26 +5,15 @@ $(function() {
   });
   $("nav#top_bar ul li a").hover(function() {
     $(this).stop().animate({
-      backgroundColor : "#77DDD1",
+      "background-color" : "#77DDD1",
     }, 100);
   }, function() {
 
     $(this).stop().animate({
-      backgroundColor : "#C4E9E0"
+      "background-color" : "#C4E9E0"
     }, 300);
   });
-  
-  /* * /
-  $("form#register input[type=button]").click(function() {
-    var paramCheck = true;
-    $("form#register select, form#register input[type=text], form#register input[type=file]").each(function() {
-      if (this.value === "")
-        paramCheck = false;
-    });
-    if (paramCheck && window.confirm("送信します。"))
-      $("form#register").submit();
-    else
-      alert("入力項目を確認してください。");
-  });
-  /* */
+  $("div#login input[type=submit]").click(function() {
+    $(this).css("background-color", "#FFFFFF");
+  })
 });
