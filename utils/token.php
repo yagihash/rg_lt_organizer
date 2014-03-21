a202c5a4bdc100e71cf01a08f26d3762b1792028
kazu1130 <nomail>
2014-03-21 21:15:04 +0900

    tokenを1セッションにつき1度だけ生成

--- utils/token.php
+++ utils/token.php
@@ -1,8 +1,10 @@
 <?php
 function issueToken() {
-  $token = bin2hex(openssl_random_pseudo_bytes(32));
-  $_SESSION["token"] = $token;
-  return $token;
+  if(!isset($_SESSION["token"])){
+    $token = bin2hex(openssl_random_pseudo_bytes(32));
+    $_SESSION["token"] = $token;
+  }
+  return $_SESSION["token"];
 }
 
 function checkToken($token) {
