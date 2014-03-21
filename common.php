d2b499e2e7fa85c9997d9169832ff1f39ab72625
Yu Yagihashi <yagihashoooooo@gmail.com>
2014-03-21 23:53:38 +0900

    ほげほげー

--- common.php
+++ common.php
@@ -1,23 +1,22 @@
 <?php
 // for debugging
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', '1');
-ini_set('session.cookie_httponly', 1);
+ini_set("error_reporting", E_ALL);
+ini_set("display_errors", "1");
+ini_set("session.cookie_httponly", 1);
 
 $utils = glob(__DIR__ . "/utils/*.php");
 foreach ($utils as $file_path)
   require_once ($file_path);
 
-require_once __DIR__ . '/lib/j4mie/idiorm.php';
-ORM::configure('mysql:dbname=rg;host=127.0.0.1;charset=utf8;');
-ORM::configure('username', 'rg');
-ORM::configure('password', 'smARFbVawcUXCKaj');
-require_once __DIR__ . '/lib/j4mie/paris.php';
+require_once (__DIR__ . "/lib/j4mie/idiorm.php");
+ORM::configure("mysql:dbname=rg;host=127.0.0.1;charset=utf8;");
+ORM::configure("username", "rg");
+ORM::configure("password", "smARFbVawcUXCKaj");
+require_once (__DIR__ . "/lib/j4mie/paris.php");
 
-spl_autoload_register(function ($class) {
-    include_once __DIR__ . '/class/' . $class . '.php';
+spl_autoload_register(function($class) {
+  include_once __DIR__ . "/class/" . $class . ".php";
 });
-
 
 $base_url = dirname($_SERVER["SCRIPT_NAME"]);
 
