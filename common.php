3ba3bb79157b4e984ea3b4ff8df0835fa81f297a
yagihashoo <yagihashoooooo@gmail.com>
2014-04-03 22:45:44 +0900

    タイムゾーンの設定

--- common.php
+++ common.php
@@ -4,6 +4,8 @@ ini_set("error_reporting", E_ALL);
 ini_set("display_errors", "1");
 ini_set("session.cookie_httponly", 1);
 
+date_default_timezone_set('Asia/Tokyo');
+
 $utils = glob(__DIR__ . "/utils/*.php");
 foreach ($utils as $file_path)
   require_once ($file_path);
