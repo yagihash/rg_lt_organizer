53d613eca48ed7b812ca26e0b7a227d0361333e3
Yu Yagihashi <yagihashoooooo@gmail.com>
2014-04-03 23:49:13 +0900

    adminページをなんとなく整形

--- admin.php
+++ admin.php
@@ -73,6 +73,7 @@ if(checkToken($token)){
 <?php
 require_once (__DIR__ . "/head.php");
 ?>
+    <link rel="stylesheet" href="css/admin.css" />
   </head>
 
   <body>
@@ -80,15 +81,20 @@ require_once (__DIR__ . "/head.php");
 require_once(__DIR__ . "/top_bar.php");
 ?>
     <div id="wrap">
-    </div>
-    <div id="main">
-          <form action="admin.php" method="POST">
+<?php
+require_once(__DIR__ . "/page_header.php");
+?>
+      <div id="main" class="admin">
+        <h2>Admin menu</h2>
+        <h3>KG</h3>
+        <div class="admin_form_box">
+          <form action="admin.php" method="POST" class="admin">
             <input type="hidden" name="token" value="<?php echo issueToken(); ?>" />
             <input type="hidden" name="mode" value="kg_add" />
-            <input type="text" name="name" placeholder="KG name" />
+            <input type="text" name="name" placeholder="New KG name" />
             <input type="submit" value="Add KG" />
           </form>
-          <form action="admin.php" method="POST">
+          <form action="admin.php" method="POST" class="admin">
             <input type="hidden" name="token" value="<?php echo issueToken(); ?>" />
             <input type="hidden" name="mode" value="kg_delete" />
             <select name="id" required>
@@ -99,10 +105,11 @@ foreach($kgs as $kg){
   echo "              <option value=\"".escapeHTML($kg->id)."\">".escapeHTML($kg->name)."</option>";
 }
 ?>
+
             </select>
             <input type="submit" value="Delete KG" />
           </form>
-          <form action="admin.php" method="POST">
+          <form action="admin.php" method="POST" class="admin">
             <input type="hidden" name="token" value="<?php echo issueToken(); ?>" />
             <input type="hidden" name="mode" value="kg_change" />
             <select name="id" required>
@@ -113,33 +120,37 @@ foreach($kgs as $kg){
   echo "              <option value=\"".escapeHTML($kg->id)."\">".escapeHTML($kg->name)."</option>";
 }
 ?>
+
             </select>
             <input type="text" name="name" placeholder="KG name" />
             <input type="submit" value="Change KG name" />
           </form>
-          <form action="admin.php" method="POST">
+        </div>
+        <h3>LT Weeks</h3>
+        <div class="admin_form_box">
+          <form action="admin.php" method="POST" class="admin">
             <input type="hidden" name="token" value="<?php echo issueToken(); ?>" />
             <input type="hidden" name="mode" value="week_add" />
-            <input type="text" name="week" pattern="^[0-9]{1,2}$" maxlength="2" placeholder="Week" />
-            <input type="text" name="date" pattern="^[0-9]{4}-\d{2}-\d{2}$" maxlength="10" placeholder="Date" /><!-- Picker -->
+            <input type="text" name="week" pattern="^[0-9]{1,2}$" maxlength="2" placeholder="1-12" />
+            <input type="text" name="date" pattern="^[0-9]{4}-\d{2}-\d{2}$" maxlength="10" placeholder="YYYY-MM-DD" /><!-- Picker -->
             <input type="submit" value="Add Week" />
           </form>
-          <form action="admin.php" method="POST">
+          <form action="admin.php" method="POST" class="admin">
             <input type="hidden" name="token" value="<?php echo issueToken(); ?>" />
             <input type="hidden" name="mode" value="week_delete" />
             <select name="id" required>
               <option value="">-----</option>
-
 <?php
 $lt_weeks = LtWeek::find_many();
 foreach($lt_weeks as $lt_week){
   echo "              <option value=\"".escapeHTML($lt_week->id)."\">第".escapeHTML($lt_week->week)."回目(".escapeHTML($lt_week->date).")</option>";
 }
 ?>
+
             </select>
             <input type="submit" value="Delete Week" />
           </form>
-          <form action="admin.php" method="POST">
+          <form action="admin.php" method="POST" class="admin">
             <input type="hidden" name="token" value="<?php echo issueToken(); ?>" />
             <input type="hidden" name="mode" value="week_change" />
             <select name="id" required>
@@ -151,11 +162,14 @@ foreach($lt_weeks as $lt_week){
   echo "              <option value=\"".escapeHTML($lt_week->id)."\">第".escapeHTML($lt_week->week)."回目(".escapeHTML($lt_week->date).")</option>";
 }
 ?>
+
             </select>
-            <input type="text" name="week" pattern="^[0-9]{1,2}$" maxlength="2" placeholder="Week" />
-            <input type="text" name="date" pattern="^[0-9]{4}-\d{2}-\d{2}$" maxlength="10" placeholder="Date" /><!-- Picker -->
+            <input type="text" name="week" pattern="^[0-9]{1,2}$" maxlength="2" placeholder="1-12" />
+            <input type="text" name="date" pattern="^[0-9]{4}-\d{2}-\d{2}$" maxlength="10" placeholder="YYYY-MM-DD" /><!-- Picker -->
             <input type="submit" value="Change Week" />
           </form>
+        </div>
+      </div>
     </div>
   </body>
 </html>
